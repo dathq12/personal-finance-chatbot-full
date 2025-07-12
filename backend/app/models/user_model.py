8b7bda2c1ba2dc79a53b90885782a441af0779ee
dathq12 <hqd.affair@gmail.com>
2025-07-12 14:27:31 +0700

    update giao diện UI

--- backend/app/models/user_model.py
+++ backend/app/models/user_model.py
@@ -5,4 +5,4 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(255), unique=True, index=True)
-    hashed_password = Column(String)
+    password_hash = Column(String)
